c83e7b04a3ce5b99867813c017e5418d8fe26712
Song Zhou <zs@ziorix.com>
2021-08-18 14:45:50 -0700

    tsconfig

---
+++ packages/ipfs/src/index.spec.ts
@@ -0,0 +1,33 @@
+// Copyright 2021 @skyekiwi/ipfs authors & contributors
+// SPDX-License-Identifier: Apache-2.0
+
+import { randomBytes } from 'tweetnacl';
+
+import { u8aToHex } from '@skyekiwi/util';
+
+import { IPFS } from '.';
+
+describe('@skyekiwi/ipfs', function () {
+  const ipfs = new IPFS();
+
+  test('ipfs works', async () => {
+    const cids = [];
+    const data = [];
+
+    for (let i = 0; i < 5; i++) {
+      data.push(randomBytes(10000));
+      const hex = u8aToHex(data[i]);
+
+      cids.push(await ipfs.add(hex));
+      expect(cids[i].size).toBeGreaterThanOrEqual(10000 * 2);
+    }
+
+    for (let i = 0; i < 5; i++) {
+      const content = await ipfs.cat(cids[i].cid);
+
+      expect(content).toEqual(u8aToHex(data[i]));
+    }
+
+    await ipfs.stopIfRunning();
+  });
+});
